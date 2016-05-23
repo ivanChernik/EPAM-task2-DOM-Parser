df03622391c884613fdd4e3a2f625c50f67f726b
Ivan Chernikau <chernik97@gmail.com>
2016-05-24 01:27:31 +0300

    Added Document and Attribute

---
+++ src/by/epam/traning_center/entity/Document.java
@@ -0,0 +1,14 @@
+package by.epam.traning_center.entity;
+
+public class Document {
+	private Element rootElement;
+	
+	public Document(Element rootElement){
+		this.rootElement = rootElement;
+	}
+
+	public Element getRootElement() {
+		return rootElement;
+	}
+
+}
